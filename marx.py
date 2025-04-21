6842c8677704c0c9a20cb8d05d8139c091cee29d
Robostew0 <joteakus@gmail.com>
2025-04-21 18:59:20 +0000

    It speaks

--- marx.py
+++ marx.py
@@ -1,6 +1,10 @@
 import requests
 import json
 import time
+import os
+from pyt2s.services import stream_elements
+from pygame import mixer
+import threading
 
 # For personal use with marx ollama model
 with open("settings.json", "r") as file:
@@ -10,11 +14,30 @@ with open("settings.json", "r") as file:
     streamMode = settings["stream"]
     model = settings["model"]
     endTokens = settings["endtokens"]
+    temperature = settings["creativity"]
+    if settings["voice"] == 1:
+        voice = "Hans"
+    else:
+        voice = "de-DE-Wavenet-B"
+
 messages = []
+
+def playSounds(soundsList):
+    mixer.init()
+    soundsPlayed = 0
+    play = True
+    while play:
+        while soundsPlayed == len(soundsList):
+            time.sleep(0.1)
+        if soundsList[soundsPlayed] == "done":
+            break
+        soundsList[soundsPlayed].play()
+        while mixer.get_busy():
+            time.sleep(0.1)
+        soundsPlayed+=1
 
 def chatbotStream(body):
     with requests.post(url, json=body, stream=streamMode) as response:
-        message = ""
         if streamMode == False:
             # If not streaming, simply print the response
             token = response.json()["message"]["content"]
@@ -22,10 +45,13 @@ def chatbotStream(body):
             return
         # If streaming, construct a series of dictionaries from the stream and print the response of each dictionary
         section = ""
+        chunkBytes = b""
         for chunk in response.iter_content():
+            # Try to decode the character in the stream and append it to the running section
+            chunkBytes = chunkBytes + chunk
             try:
-                # Try to decode the character in the stream and append it to the running section
-                character = chunk.decode('utf-8')
+                character = chunkBytes.decode('utf-8')
+                chunkBytes = b""
                 section = section + character
                 # If it's a newline, a new json object has begun and the token can be printed
                 if character == '\n':
@@ -34,7 +60,6 @@ def chatbotStream(body):
                     section = ""
                     yield token
             except:
-                # Ignore bad characters
                 pass
 
 print("\nEnter \"quit\" to exit\nChatting with Karlie\n...")
@@ -55,21 +80,48 @@ while True:
     body = {
         "model": model,
         "messages": messages,
-        "stream": streamMode
+        "stream": streamMode,
+        "options": {
+            "seed": round(time.time()),
+            "temperature": temperature
+        }
     }
 
+    mixer.init()
 
     textStream = chatbotStream(body)
     fullMessage = ""
     tokenBuffer = ""
+
+    sound = 0
+    sounds = []
+
+    thread = threading.Thread(target=playSounds, args=(sounds,))
+    thread.start()
+
     for token in textStream:
         tokenBuffer = tokenBuffer + token
         fullMessage = fullMessage + token
         print(token, end="", flush=True)
-        if token in endTokens:
-            # Do TTS on tokenBuffer here
+        if token in endTokens or streamMode == False:
+            sound+=1
+            data = stream_elements.requestTTS(text=tokenBuffer, voice=voice)  #de-DE-Standard-B
+            try:
+                os.remove(f"ttsSound{sound}.mp3")
+            except:
+                pass
+
+            with open(f"ttsSound{sound}.mp3", 'wb') as file:
+                file.write(data)
+                try:
+                    sounds.append(mixer.Sound(f"ttsSound{sound}.mp3"))
+                except:
+                    pass
+            os.remove(f"ttsSound{sound}.mp3")
+
             tokenBuffer = ""
 
+    sounds.append("done")
 
     # Add the whole message to the history
     messages.append(
